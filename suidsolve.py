cb17c37747f3d5beb8ebb4a57286a81cb20a6239
T. F <tjf952@gmail.com>
2024-05-22 08:47:26 -0700

    sample file;

---
+++ suidsolve.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""_title_
+
+_description_
+
+Usage: $ python3 suidsolve.py <_args_>
+
+Arguments:
+    -_param_ <type>: _description_
+"""
+
+### Import Statements ###
+
+import os
+
+print(f"Hello {os.getlogin()}")
+
+
+### Functions ###
+
+
+def function1(_param_: type) -> type:
+    """_description_
+
+    Args:
+        _param_ (type): _description_
+
+    Returns:
+        type: _description_
+    """
+    return _param_
+
+
+if __name__ == "__main__":
+    print("This is a template python file.")
